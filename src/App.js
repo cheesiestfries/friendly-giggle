c9d53c5a0c5cbff6f77c023c91351f951af25fa8
cheesiestfries <125315399+cheesiestfries@users.noreply.github.com>
2023-11-27 20:26:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const datayay = [
+  { fruit: 'Apple', anshi: 4000, muralini: 2400},
+  { fruit: 'Banana', anshi: 300, muralini: 1398},
+  { fruit: 'Cucumber', anshi: 2000, muralini: 9800},
+  { fruit: 'Dates', anshi: 9000, muralini: 3908},
+  { fruit: 'Eggplant', anshi: 300, muralini: 4800},
+  { fruit: 'Fig', anshi: 2390, muralini: 200},
+  { fruit: 'Grapes', anshi: 9000, muralini: 9000}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   
+      <header className="app-headers">
+        <h1>how much anshi and muralini like fruit and veggies!!!!!!!!!!</h1>
+        <div>
+          <BarChart width={window.innerWidth - 100} height={window.innerHeight - 100} data={datayay}>
+          <CartesianGrid/>
+            <XAxis dataKey="fruit" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="anshi" fill="#9a1d2e" />
+            <Bar dataKey="muralini" fill="#546E7A" />
+          </BarChart>
+        </div>
       </header>
-    </div>
+
   );
 }
 
